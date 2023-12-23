23c8125e8f8ee0c55ef6775fdcd4203cf4a875e5
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-23 20:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,28 +30,6 @@ def audio(video_id):
 
     return "Invalid or missing YouTube link parameter."
 
-@app.route('/download')
-def download():
-  # Get the URL of the YouTube video
-  url = request.args.get('url')
-  format = request.args.get('format')
-  
-  yt = YouTube(url)
-  
-  if format == 'mp4':
-    # Download the video
-    video = yt.streams.get_highest_resolution()
-    file_stream = io.BytesIO()
-    video.stream_to_buffer(file_stream)
-    file_stream.seek(0)
-  elif format == 'mp3':
-    mp3_stream = yt.streams.get_audio_only()
-    file_stream = io.BytesIO()
-    mp3_stream.stream_to_buffer(file_stream)
-    file_stream.seek(0)
-
-  # Send the file to the browser
-  return send_file(file_stream, as_attachment=True, download_name=f'{yt.title}.{format}')
 
 
 @app.route("/video", methods=["GET"])
